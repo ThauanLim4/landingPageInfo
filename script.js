1a49e78e0a86f01e6ab1de36160279165e2904f7
Thauan Lim'as <116463820+ThauanLim4@users.noreply.github.com>
2024-04-08 19:44:28 -0300

    att 3

--- script.js
+++ script.js
@@ -1,14 +1,24 @@
-// $(document).ready(function(){
-//     $('.menuButton').on('click', function(){
-//         $('#menu').toggleClass('active')
-//         $('.menuButton').toggleClass('fa-x')
-//     })
-// })
-
 const botaoMenu = document.querySelector('.menuButton')
 
 botaoMenu.addEventListener('click', function(){
     let menu = document.getElementById('menu')
     menu.classList.toggle('active')
     botaoMenu.classList.toggle('fa-x')
+    botaoMenu.style.transition = 'all ease-in 0.2s'
+})
+
+const categorias = document.getElementById('categorias')
+const recommendedProducts = document.getElementById('recommendedProducts')
+
+const observer =  new IntersectionObserver(entries => {
+    const itens = document.querySelectorAll('.headerListItem')
+    entries.forEach(entrye => {
+        entrye.target.classList.toggle('inView', entrye.isIntersecting)
+    })
+})
+const sections = document.querySelectorAll('section')
+console.log(sections)
+
+sections.forEach(itens => {
+    observer.observe(itens)
 })
